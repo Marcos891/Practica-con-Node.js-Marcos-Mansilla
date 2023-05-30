5a4b535909097104b6decc00b17a4d8bb32fd2b6
Marcos891 <MarcosMansilla1989@hotmail.com>
2023-05-30 16:12:50 -0300

    Ejercicio FizzBuzz creado y chequeado

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,1 +1,22 @@
-a
+function multiplo(N) {
+    const ArrMultiplo = [];
+    for (let i = 1; i <= N; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+             ArrMultiplo.push("FizzBuzz");
+         }
+        else if (i % 3 === 0) {
+            ArrMultiplo.push("Fizz");
+        }
+        else if (i % 5 === 0) {
+             ArrMultiplo.push("Buzz");
+        }
+        else
+         {
+         ArrMultiplo.push(i);
+         }
+    };
+    return ArrMultiplo;
+};
+
+console.log(multiplo(N));
+module.exports = multiplo;
