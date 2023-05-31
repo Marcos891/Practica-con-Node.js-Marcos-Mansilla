c9c7ee9183cf2192ed029f25e27bccc3ffa574c6
Marcos891 <MarcosMansilla1989@hotmail.com>
2023-05-30 22:01:14 -0300

    Funcion de numeros carinality hecho y testeado

--- numberCardinality.js
+++ numberCardinality.js
@@ -1,1 +1,18 @@
-a
+function nCardinality(N) {
+    let num = N % 10;
+    if (num === 0) {
+        return "zero"
+    }
+    else if(num === 5){
+        return "five"
+    }
+    else if(num % 2 === 0){
+        return "even"
+    }
+    else{
+        return "odd"
+    }
+}
+
+console.log(nCardinality(N))
+module.exports = nCardinality;
